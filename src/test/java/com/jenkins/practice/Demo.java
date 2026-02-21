99a57c15954d6db62ca7c11465e2298619439f20
dharmendrah789@gmail.com <dharmendrah789@gmail.com>
2026-02-21 21:58:48 +0530

    my second commit

--- src/test/java/com/jenkins/practice/Demo.java
+++ src/test/java/com/jenkins/practice/Demo.java
@@ -1,10 +1,19 @@
 package com.jenkins.practice;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class Demo {
 
 	public static void main(String[] args) {
 
-      System.out.println("Hello");
+		WebDriverManager.chromedriver().setup(); // in place of System.seProprty("", "");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		driver.manage().window().maximize();
 
 	}
 
