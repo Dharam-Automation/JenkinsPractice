bbf8858fd0afc857a9c981ba3dbfd9f9a153565a
dharmendrah789@gmail.com <dharmendrah789@gmail.com>
2026-02-23 22:09:48 +0530

    testng test added

--- src/test/java/com/jenkins/practice/NewTest.java
+++ src/test/java/com/jenkins/practice/NewTest.java
@@ -2,19 +2,20 @@ package com.jenkins.practice;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class Demo {
-
-	public static void main(String[] args) {
-
+public class NewTest {
+  @Test
+  public void f() {
+	  
 		WebDriverManager.chromedriver().setup(); // in place of System.seProprty("", "");
 		WebDriver driver = new ChromeDriver();
 		
 		driver.get("https://opensource-demo.orangehrmlive.com/");
 		driver.manage().window().maximize();
+		driver.close();
 
-	}
-
+  }
 }
